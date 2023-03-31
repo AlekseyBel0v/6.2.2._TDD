0ed159f7d6e94b85b9d670be7620531ce6923df7
Alexey Belov <bel0valeksey@yandex.ru>
2023-03-31 18:07:47 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,18 @@
 public class PhoneBook {
+
+    int add(String name, String number) throws Exception{
+        return 0;
+    }
+
+    String findByNumber(String number) {
+        return "";
+    }
+
+    String findByName(String name) {
+        return "";
+    }
+
+    void printAllNames() {
+
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    void testByAdditionNewContact() throws Exception {
+        String name = "Petya";
+        String phoneNumber = "1111";
+        int expectedQuantityOfContacts = 1;
+
+        int quantityOfContacts = phoneBook.add(name, phoneNumber);
+
+        Assertions.assertEquals(expectedQuantityOfContacts, quantityOfContacts);
+    }
+
+    @Test
+    void testByAdditionDoubleContact() {
+        String name = "Petya";
+        String phoneNumber = "1111";
+
+        Executable executable = () -> {
+            phoneBook.add(name, phoneNumber);
+            phoneBook.add(name, phoneNumber);
+        };
+
+        Assertions.assertThrows(Exception.class, executable);
+    }
 }
